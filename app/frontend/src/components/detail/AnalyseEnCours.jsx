5180e48f9c33bdce323aede31bf760dbdba09faf
Julien <jlarose72@gmail.com>
2026-06-25 11:51:59 -0400

    Ajuste le message d'attente (l'analyse exhaustive prend plus d'une minute)

--- app/frontend/src/components/detail/AnalyseEnCours.jsx
+++ app/frontend/src/components/detail/AnalyseEnCours.jsx
@@ -4,7 +4,7 @@ export default function AnalyseEnCours() {
   return (
     <div className="card" style={{ textAlign: 'center', padding: '3rem 1.5rem' }}>
       <Spinner taille="grand" />
-      <p>Analyse en cours, ceci peut prendre une minute.</p>
+      <p>Analyse en cours, ceci peut prendre quelques minutes.</p>
     </div>
   )
 }
